6a3568a2e1aad0c7b0f199ded8937a84b4667eac
Tony DiNitto <think41c@gmail.com>
2015-03-23 09:02:03 -0600

    All tests passing. Refactoring is needed for conciseness.

--- raindrops.rb
+++ raindrops.rb
@@ -1,11 +1,15 @@
 class Raindrops
   def self.convert(x)
-    if    (x % 7 == 0) && (x % 5 != 0)
+    if    (x % 7 == 0) && (x % 3 == 0) && (x % 5 == 0)
+      "PlingPlangPlong"
+    elsif (x % 7 == 0) && (x % 3 == 0)
+      "PlingPlong"
+    elsif (x % 7 == 0) && (x % 5 != 0)
       "Plong"
     elsif (x % 3 == 0) && (x % 5 == 0)
       "PlingPlang"
     elsif (x % 7 == 0) && (x % 5 == 0) 
-      "PlingPlong"
+      "PlangPlong"
     elsif (x % 3 == 0) && (x % 5 != 0)
       "Pling"
     elsif (x % 5 == 0) 
@@ -16,7 +20,9 @@ class Raindrops
   end
 end
 
+if __FILE__ == $0 
+
 a = Raindrops.new
 puts  Raindrops.convert(3)
+end
 
-puts "hi"
