97a026e7453f6d95f2a41f6576604eaba50e832e
Tony DiNitto <think41c@gmail.com>
2015-03-23 08:56:05 -0600

    More tests passing. Basic logic implemeneted.

--- raindrops.rb
+++ raindrops.rb
@@ -1,6 +1,22 @@
 class Raindrops
   def self.convert(x)
-    "1"
+    if    (x % 7 == 0) && (x % 5 != 0)
+      "Plong"
+    elsif (x % 3 == 0) && (x % 5 == 0)
+      "PlingPlang"
+    elsif (x % 7 == 0) && (x % 5 == 0) 
+      "PlingPlong"
+    elsif (x % 3 == 0) && (x % 5 != 0)
+      "Pling"
+    elsif (x % 5 == 0) 
+      "Plang"
+    else
+      x.to_s
+    end
   end
-
 end
+
+a = Raindrops.new
+puts  Raindrops.convert(3)
+
+puts "hi"
--- raindrops_test.rb
+++ raindrops_test.rb
@@ -9,42 +9,34 @@ class RaindropsTest < Minitest::Test
   end
 
   def test_3
-    skip
     assert_equal 'Pling', Raindrops.convert(3)
   end
 
   def test_5
-    skip
     assert_equal 'Plang', Raindrops.convert(5)
   end
 
   def test_7
-    skip
     assert_equal 'Plong', Raindrops.convert(7)
   end
 
   def test_6
-    skip
     assert_equal 'Pling', Raindrops.convert(6)
   end
 
   def test_9
-    skip
     assert_equal 'Pling', Raindrops.convert(9)
   end
 
   def test_10
-    skip
     assert_equal 'Plang', Raindrops.convert(10)
   end
 
   def test_14
-    skip
     assert_equal 'Plong', Raindrops.convert(14)
   end
 
   def test_15
-    skip
     assert_equal 'PlingPlang', Raindrops.convert(15)
   end
 
