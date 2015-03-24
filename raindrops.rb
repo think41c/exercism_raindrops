2531c3c5a12229e7710b8aa000570f373662aaae
Tony DiNitto <think41c@gmail.com>
2015-03-24 10:08:26 -0600

    Refactored significantly.

--- raindrops.rb
+++ raindrops.rb
@@ -1,28 +1,10 @@
 class Raindrops
   def self.convert(x)
-    if    (x % 7 == 0) && (x % 3 == 0) && (x % 5 == 0)
-      "PlingPlangPlong"
-    elsif (x % 7 == 0) && (x % 3 == 0)
-      "PlingPlong"
-    elsif (x % 7 == 0) && (x % 5 != 0)
-      "Plong"
-    elsif (x % 3 == 0) && (x % 5 == 0)
-      "PlingPlang"
-    elsif (x % 7 == 0) && (x % 5 == 0) 
-      "PlangPlong"
-    elsif (x % 3 == 0) && (x % 5 != 0)
-      "Pling"
-    elsif (x % 5 == 0) 
-      "Plang"
-    else
-      x.to_s
-    end
+    result = []
+    result << "Pling" if  x % 3 == 0 
+    result << "Plang" if  x % 5 == 0 
+    result << "Plong" if  x % 7 == 0
+    result << x       if result == []
+    result.join
   end
 end
-
-if __FILE__ == $0 
-
-a = Raindrops.new
-puts  Raindrops.convert(3)
-end
-
