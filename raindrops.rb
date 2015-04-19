0fa30e788e5d03a28233fe24172d857da1abd51b
Tony DiNitto <think41c@gmail.com>
2015-04-18 23:37:18 -0600

    Refactor possibility for exercism nitpickers.

--- raindrops.rb
+++ raindrops.rb
@@ -2,33 +2,20 @@ class Raindrops
   def self.convert(x)
     answer = []
     result = {}
-    result = { 3 => "Pling" } 
-    result = { 5 => "Plang" }
-    result = { 7 => "Plong" }
+    result[3] = "Pling"
+    result[5] = "Plang"
+    result[7] = "Plong"
     result.map do |num, word|
       if x % num == 0 then
         answer << word
-      else
-        answer << num
       end
-
-      answer
-      end
-
-# Old method
-    # result = []
-    # result << "Pling" if  x % 3 == 0 
-    # result << "Plang" if  x % 5 == 0 
-    # result << "Plong" if  x % 7 == 0
-    # result << x       if result == []
-    # result.join
-
-
-# new idea 
-    # result = hash.map do |number, word|
-    # word if x % number == 0
-    # end
-
+    end
+    if answer.size == 0 
+      answer = x
+      answer.to_s
+    else
+      answer.join
+    end
   end
 end
-p Raindrops.convert(1209)
+p Raindrops.convert(5)
