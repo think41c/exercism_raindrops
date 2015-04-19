44a25dc63ac1ce4d28567b004b1b06e28c838254
Tony DiNitto <think41c@gmail.com>
2015-04-18 23:40:19 -0600

    Changed formatting. Eliminated unused vars.

--- raindrops.rb
+++ raindrops.rb
@@ -2,20 +2,23 @@ class Raindrops
   def self.convert(x)
     answer = []
     result = {}
+ 
     result[3] = "Pling"
     result[5] = "Plang"
     result[7] = "Plong"
+    
     result.map do |num, word|
       if x % num == 0 then
         answer << word
       end
     end
+    
     if answer.size == 0 
       answer = x.to_s
-      answer.to_s
     else
       answer.join
     end
+
   end
 end
 p Raindrops.convert(5)
